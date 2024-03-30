87f5f7688de685edcb4575a11bd9f0a8c207545a
Michael Bishop <cleverca22@gmail.com>
2024-03-30 02:19:51 -0300

    fix some bugs

--- gang-finder/gang-finder.cpp
+++ gang-finder/gang-finder.cpp
@@ -18,7 +18,7 @@ void recurse(DIR *hnd) {
   while (ent = readdir(hnd)) {
     if (strcmp(ent->d_name, ".") == 0) continue;
     if (strcmp(ent->d_name, "..") == 0) continue;
-    if (ent->d_type & DT_DIR) {
+    if (ent->d_type == DT_DIR) {
       int dirfd = openat(fd, ent->d_name, O_CLOEXEC | O_DIRECTORY | O_NOCTTY | O_RDONLY);
       if ((dirfd == -1) && (errno == EACCES)) continue;
       assert(dirfd >= 0);
@@ -28,12 +28,17 @@ void recurse(DIR *hnd) {
     } else {
       struct stat statbuf;
       int ret = fstatat(fd, ent->d_name, &statbuf, AT_SYMLINK_NOFOLLOW);
+      if (ret == -1) {
+        if (errno == ENOENT) continue;
+      }
       assert(ret == 0);
       if ((statbuf.st_blocks * 512) > statbuf.st_size) {
         //printf("inode %ld, sizes: %ld %ld\n", statbuf.st_ino, statbuf.st_size, statbuf.st_blocks * 512);
         uint64_t overhead = (statbuf.st_blocks * 512) - statbuf.st_size;
-        printf("%ldMb %s\n", overhead/1024/1024, ent->d_name);
-        total += overhead;
+        if (overhead > (1 << 20)) {
+          printf("%ldMb %s\n", overhead/1024/1024, ent->d_name);
+          total += overhead;
+        }
       }
     }
   }
